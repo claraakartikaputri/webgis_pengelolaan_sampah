c5f54f7800c5177de30fb63aad29955642c03fa8
clara kartika putri <claraakartikaputri@gmail.com>
2026-06-02 12:02:13 +0700

    fix env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://ogdrmzvypubtgachkcvq.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_SHbYKvMVeB21Q8jrX1fFng_OIN_SJaT";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
